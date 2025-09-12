9cca2bc4f9ae704c589b88445778943a63ab3481
raph <raphael.agramunt@epitech.eu>
2025-09-12 17:46:21 +0200

    [+] adding a button(linked to nothing for now) and adding a new font and applying it plus modif on main page

--- epitrello/app/(marketing)/page.tsx
+++ epitrello/app/(marketing)/page.tsx
@@ -1,9 +1,21 @@
+import Link from "next/link";
+import localFont from"next/font/local";
 import { Medal } from "lucide-react";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+const headingFont = localFont({
+    src: "../../public/fonts/CalSans-Regular.ttf"
+});
 
 const MarketingPage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center flex-col">
+            <div className={cn(
+                "flex items-center justify-center flex-col",
+                headingFont.className  
+            )}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-blue-700 text-black rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2"/>
                     No 1 task management
@@ -18,6 +30,11 @@ const MarketingPage = () => {
             <div className="text-sm md:text-xl text-black mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
                 Colaborate, manage project and reach new productivity peaks. From high rises to the home office, the way your team works is unique - accomplish it all with EpiTrello.
             </div>
+            <Button className="mt-6" size={"lg"} asChild>
+                <Link href="/sign-up">
+                    Get started for free
+                </Link>
+            </Button>
         </div>
     );
 };
