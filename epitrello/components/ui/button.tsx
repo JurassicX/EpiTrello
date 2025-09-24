ab3566e850dc186a6ab8311dabf193d36be601ed
raph <raphael.agramunt@epitech.eu>
2025-09-24 17:04:56 +0200

    [+] slight change of color for the primary variant of button and applying it on the /organization button

--- epitrello/components/ui/button.tsx
+++ epitrello/components/ui/button.tsx
@@ -22,7 +22,7 @@ const buttonVariants = cva(
         link:
           "text-primary underline-offset-4 hover:underline",
         primary:
-          "bg-sky-700 text-primary-foreground hover:bg-sky-700/90"        
+          "bg-blue-900 text-primary-foreground hover:bg-blue-800/90"        
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
