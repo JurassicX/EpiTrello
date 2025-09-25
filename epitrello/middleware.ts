8c3180a9ab718121cc4a85a55d6a7710860e2901
raph <raphael.agramunt@epitech.eu>
2025-09-25 17:00:03 +0200

    [+] now the middleware block access to logged out only route when user is logged in

--- epitrello/middleware.ts
+++ epitrello/middleware.ts
@@ -1,13 +1,26 @@
+import { NextResponse } from 'next/server';
+
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
     '/sign-in(.*)',
     '/sign-up(.*)',
     '/',
-])
+]);
+
+const isLoggedOutOnlyRoute = createRouteMatcher([
+    '/sign-in(.*)',
+    '/sign-up(.*)',
+    '/',
+]);
 
 export default clerkMiddleware(async (auth, req) => {
-    if (!isPublicRoute(req)) {
+    const { userId } = await auth();
+
+    if (isLoggedOutOnlyRoute(req) && userId) {
+        return NextResponse.redirect(new URL('/select-org', req.url));
+    }
+    if (!isPublicRoute(req) && !userId) {
       await auth.protect()
     }
 });
