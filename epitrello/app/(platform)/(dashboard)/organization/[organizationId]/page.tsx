3fd82d46e9ec4ebe48cee065a49d8af39cb14f3b
raph <raphael.agramunt@epitech.eu>
2025-10-02 00:01:27 +0200

    [+] upgrading the organization page with a input bar  and the base of the code to get data from the db

--- epitrello/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ epitrello/app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,7 +1,29 @@
+import { db } from "@/lib/db";
+
 const OrganizationIdPage = () => {
+    async function create(formData: FormData) {
+        "use server";
+
+        const title = formData.get("title") as string;
+
+        db.board.create({
+            data: {
+                title,
+            }
+        });
+    }
+
     return (
-        <div>
-            organisation page
+        <div className="w-full">
+            <form action={create} className="">
+                <input
+                    id="title"
+                    name="title"
+                    required
+                    placeholder=" Search a board."
+                    className="w-full border-black border rounded-sm p-1"
+                />
+            </form>
         </div>
     );
 };
