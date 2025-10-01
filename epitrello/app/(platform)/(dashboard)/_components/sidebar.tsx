87fbb404ad4b689ade19e130ead9e3c49b1cdb47
raph <raphael.agramunt@epitech.eu>
2025-10-01 10:28:37 +0200

    [+] while sidebar is loading show skeleton for better immersion

--- epitrello/app/(platform)/(dashboard)/_components/sidebar.tsx
+++ epitrello/app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -57,7 +57,13 @@ export const Sidebar = ({
     if (!isLoadedOrganization || !isLoadedOrganizationList || userMemberships.isLoading) {
         return (
             <>
-                <Skeleton />
+                <div className="flex items-center justify-between mb-2">
+                    <Skeleton className="h-10 w-[50%]"/>
+                    <Skeleton className="h-10 w-10"/>
+                </div>
+                <div className="space-y-2">
+                    <NavItem.Skeleton />
+                </div>
             </>
         );
     };
