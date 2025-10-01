f3b8f7daa70e9a3c562f16244ba833fe69b516ee
raph <raphael.agramunt@epitech.eu>
2025-10-01 10:25:05 +0200

    [+] creting a skeleton for nav-item components

--- epitrello/app/(platform)/(dashboard)/_components/nav-items.tsx
+++ epitrello/app/(platform)/(dashboard)/_components/nav-items.tsx
@@ -13,6 +13,7 @@ import {
 import { cn } from "@/lib/utils";
 import { AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
     id: string;
@@ -108,5 +109,16 @@ export const NavItem = ({
                 ))}
             </AccordionContent>
         </AccordionItem>
+    );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+    return (
+        <div className="flex items-centerk gap-x-2">
+            <div className="w-10 h-10 relative shrink-0">
+                <Skeleton className="h-full w-full absolute"/>
+            </div>
+            <Skeleton className="h-10 w-full"/>
+        </div>
     );
 };
