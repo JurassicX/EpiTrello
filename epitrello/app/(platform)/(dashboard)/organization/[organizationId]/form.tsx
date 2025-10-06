103dfe7c08d6e76c5b17320fd5c208bcb5e88241
raph <raphael.agramunt@epitech.eu>
2025-10-06 11:49:54 +0200

    [-] deleting the old form-input.tsx components to use the one in @/components/form/ therefore changing the call in form.tsx

--- epitrello/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ epitrello/app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -1,9 +1,9 @@
 "use client";
 
 import { createBoard } from "@/actions/create-board";
-
-import { FormInput } from "./form-input";
+import { FormInput } from "@/components/form/form-input";
 import { useAction } from "@/hooks/use-action";
+import { FormButton } from "./form-button";
 
 export const Form = () => {
     const { execute, fieldErrors} = useAction(createBoard, {
@@ -24,8 +24,13 @@ export const Form = () => {
     return (
         <form action={onSubmit}>
             <div className="flex flex-col space-y-2">
-                <FormInput errors={fieldErrors}/>
+                <FormInput
+                    label="Board Title"
+                    errors={fieldErrors}
+                    id="title"
+                />
             </div>
+            <FormButton />
         </form>
     );
 };
