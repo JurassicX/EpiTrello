d325eed2bf175b86f3e2c3ef3cf279fe460f7508
raph <raphael.agramunt@epitech.eu>
2025-10-07 11:51:43 +0200

    [+] applying a formPopover component in board-list.tsx that wrap the button create new board

--- epitrello/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ epitrello/app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,6 +1,7 @@
 import { HelpCircle, User2 } from "lucide-react";
 
 import { Hint } from "@/components/hint";
+import { FormPopover } from "@/components/form/form-popover";
 
 export const Boardlist = () => {
     return (
@@ -10,27 +11,29 @@ export const Boardlist = () => {
                 Your Boards
             </div>
             <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-                <div 
-                    role="button"
-                    className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
-                >
-                    <p className="text-sm">
-                        Create new board
-                    </p>
-                    <span className="text-xs">
-                        5 remaining
-                    </span>
-                    <Hint
-                        sideOffset={40}
-                        description={`
-                            Free Workspaces can have up to 5 boards. For unlimited boards, upgrade this Workspace to Premium.
-                        `}
+                <FormPopover sideOffset={10} side="right">
+                    <div 
+                        role="button"
+                        className="aspect-video relative h-full w-full bg-muted rounded-sm flex flex-col gap-y-1 items-center justify-center hover:opacity-75 transition"
                     >
-                        <HelpCircle 
-                            className="absolute bottom-2 right-2 h-[14px] w-[14px]"
-                        />
-                    </Hint>
-                </div>
+                        <p className="text-sm">
+                            Create new board
+                        </p>
+                        <span className="text-xs">
+                            5 remaining
+                        </span>
+                        <Hint
+                            sideOffset={40}
+                            description={`
+                                Free Workspaces can have up to 5 boards. For unlimited boards, upgrade this Workspace to Premium.
+                            `}
+                        >
+                            <HelpCircle 
+                                className="absolute bottom-2 right-2 h-[14px] w-[14px]"
+                            />
+                        </Hint>
+                    </div>
+                </FormPopover>
             </div>
         </div>
     );
